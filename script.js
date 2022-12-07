ff86cae304fe48615bdcfc33aaa435e931dd2348
SiarheiSychou <siarheisychou1999@gmail.com>
2022-12-08 00:30:22 +0400

    cleaned code

--- script.js
+++ script.js
@@ -17,21 +17,3 @@ function equalSides(array) {
     }
     return -1;
 }
-        // let leftArrSum = 0;
-        // let rightArrSum = 0;
-        // leftArr = array.slice(0, i);
-        // for (let j = 0; j < leftArr.length; j++) { 
-        //     leftArrSum += leftArr[j];
-        // }
-        // rightArr = array.slice(i + 1, array.length);
-        // for (let k = 0; k < rightArr.length; k++) { 
-        //     rightArrSum += rightArr[k];
-        // }
-        // if (leftArrSum === rightArrSum) {
-        //     return i;
-        // }
-//     }
-//     return -1;
-// }   
-
-console.log(equalSides([1, 2, 3, 4, 3, 2, 1]))
